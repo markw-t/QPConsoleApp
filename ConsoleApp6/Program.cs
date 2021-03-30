7cdf71f4325248e4d43f69da1cd31d9be62bd184
Mark Wilson-Thomas <mwthomas@microsoft.com>
2021-03-30 09:34:49 -0700

    Removing body material for start of QP

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,36 +1,12 @@
 ﻿using System;
 
-
 namespace QuickPulseConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            StringBuilder sb = new StringBuilder();
-            foreach (string s in args)
-            {
-                sb.Append(s);
-                sb.Append(",");
-            }
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.WriteLine(sb);
         }
 
-        static string[] StripBadWord(string[] args, string wordToStrip)
-        {
-            string[] vs = new string[args.Length];
-            int j = 0;
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i] == wordToStrip)
-                {
-                    vs[j++] = args[i];
-                }
-            }
-            Array.Resize(ref vs, j);
-            return vs;
-        }
+    }
 }
